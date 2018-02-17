8a155f908a5fa313bb7785890c316e5f749c3eba
Dean Moore <deanmoore009@gmail.com>
2018-02-17 17:37:58 +0000

    Uploading to GitHub

---
+++ Snake/Snake/SnakeForm.Designer.cs
@@ -0,0 +1,70 @@
+﻿namespace Snake
+{
+    partial class SnakeForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblMenu = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblMenu
+            // 
+            this.lblMenu.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lblMenu.Location = new System.Drawing.Point(0, 0);
+            this.lblMenu.Name = "lblMenu";
+            this.lblMenu.Size = new System.Drawing.Size(300, 200);
+            this.lblMenu.TabIndex = 0;
+            this.lblMenu.Text = "Snake Clone\r\nCreated by Dean Moore\r\n\r\nArrow Keys change direction\r\nYou can use th" +
+    "e space bar to pause\r\n\r\n---PRESS ENTER TO BEGIN---\r\n";
+            this.lblMenu.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblMenu.Click += new System.EventHandler(this.lblMenu_Click);
+            // 
+            // SnakeForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(300, 200);
+            this.Controls.Add(this.lblMenu);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(316, 239);
+            this.MinimizeBox = false;
+            this.Name = "SnakeForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Snake - Score: 1";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.MainForm_Paint);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.MainForm_KeyDown);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblMenu;
+    }
+}
+
